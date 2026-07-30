830a417ab382f8ab4f957f65c43838afe13da7d3
VivianaHarrizonM <viviana.harrizon@gmail.com>
2026-07-30 12:50:20 -0600

    Add the new couse the java

---
+++ 1javaGabriel/hola-mundo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    byte age = 23;
+    double price = 19.99F;
+
+    System.out.println("price = " + price);
+    System.out.println("age = " + age);
+  }
+}
