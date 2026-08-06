d9a47320672f1774fe920346f75f8b1ab0db4068
VivianaHarrizonM <viviana.harrizon@gmail.com>
2026-08-06 11:09:18 -0600

    Add ciclos

--- 1javaGabriel/hola-mundo/src/condicionales/condicionales.java
+++ 1javaGabriel/hola-mundo/src/condicionales/condicionales.java
@@ -1,5 +1,30 @@
 package condicionales;
 
 public class condicionales {
+  public static void  main()
+  {
+    int age = 17;
 
+    if (age > 18) {
+      System.out.println("Es mayor a 18");
+    } else {
+      System.out.println("Es menor a 18");
+    }
+
+    int day = 3;
+    switch (day){
+      case 1:
+        System.out.println("lunes");
+        break;
+      case 2:
+        System.out.println("martes");
+        break;
+      case 3:
+        System.out.println("miercoles");
+        break;
+      default:
+        System.out.println("El dato ongresado no es valido");
+        break;
+    }
+  }
 }
