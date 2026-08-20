dbfedba0c2c95e6e3302c26e172b5bc37e1291fd
VivianaHarrizonM <viviana.harrizon@gmail.com>
2026-08-20 11:20:22 -0600

    add conexion whit bd

---
+++ ControlClientes/src/main/java/datos/ClienteDAO.java
@@ -0,0 +1,41 @@
+
+package datos;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Cliente;
+
+/**
+ *
+ * @author viviana-harrizon
+ */
+public class ClienteDAO {
+    //CRUD -Create -Read -Update -Delete
+    private static final String SQL_SELECT = 
+            "SELECT id_cliente, nombre, apellido, email, telefono, saldo FROM clientes";
+    
+    public List<Cliente> listar(){
+        List<Cliente> clientes = new ArrayList<>();
+        try(Connection conn = Conexion.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(SQL_SELECT);
+                ResultSet rs = stmt.executeQuery()
+        ){
+        while(rs.next()){
+            Cliente cliente = new Cliente(
+                    rs.getInt("id_cliente"),
+                    rs.getString("nombre"),
+                    rs.getString("apellido"),
+                    rs.getString("email"),
+                    rs.getString("telefono"),
+                    rs.getDouble("saldo")
+            );
+            clientes.add(cliente);
+        }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+        return clientes;
+    }
+}
---
+++ ControlClientes/src/main/webapp/clientes.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : clientes
+    Created on : 19 ago 2026, 12:56:59
+    Author     : viviana-harrizon
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Control de clientes</title>
+    </head>
+    <body>
+        <h1>Control de clientes</h1>
+    </body>
+</html>
